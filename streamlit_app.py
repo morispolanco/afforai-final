6785f63e6a576e6d61cd933a79514ce0c44daca3
morispolanco <morispolanco@gmail.com>
2023-12-25 03:05:24 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,5 @@
 import streamlit as st
 import requests
-from googleapiclient.discovery import build
-from googletrans import Translator
 import json
 
 # Load Afforai API credentials
@@ -10,14 +8,8 @@ session_id = "65489d7c9ad727940f2ab26f"
 
 # Define the function to answer questions about the laws of Guatemala
 def get_legal_information(question):
-    # Initialize the Google Cloud Translation client
-    translator = Translator()
-
-    # Translate the question to Spanish
-    translated_question = translator.translate(question, dest="es").text
-
     # Construct the query for the Afforai API
-    query = f"leyes guatemala {translated_question}"
+    query = f"leyes guatemala {question}"
 
     # Make the request to the Afforai API
     response = requests.post(
@@ -44,9 +36,9 @@ def get_legal_information(question):
     return answer
 
 # Start the Streamlit application
-st.title("Preguntas sobre las leyes de Guatemala")
-st.write("Ingrese su pregunta:")
-question = st.text_input("Pregunta")
+st.title("Questions about the laws of Guatemala")
+st.write("Enter your question:")
+question = st.text_input("Question")
 
 # Answer the question
 legal_information = get_legal_information(question)
@@ -55,4 +47,4 @@ legal_information = get_legal_information(question)
 if legal_information:
     st.success(legal_information)
 else:
-    st.warning("No se encontró información legal para la pregunta proporcionada.")
+    st.warning("No legal information found for the provided question.")
