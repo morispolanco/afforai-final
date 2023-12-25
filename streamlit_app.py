48cebbd24888a8a32f010e74805ba8e4c9ac2a62
morispolanco <morispolanco@gmail.com>
2023-12-25 03:14:00 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-
+```python
 import streamlit as st
 import requests
 from googleapiclient.discovery import build
@@ -8,16 +8,16 @@ import json
 api_key = "fcbfdfe8-e9ed-41f3-a7d8-b6587538e84e"
 session_id = "65489d7c9ad727940f2ab26f"
 
-# Define la función para buscar la información de la ley
-def get_law_information(law_name):
+# Define la función para responder preguntas sobre las leyes de Guatemala
+def get_answer(question):
     # Inicializa el cliente de Google Cloud Translation
     translation_client = build("translate", "v3")
 
-    # Traducir el nombre de la ley al español
-    translated_name = translation_client.translate(law_name, target="es").get("translatedText")
+    # Traducir la pregunta al español
+    translated_question = translation_client.translate(question, target="es").get("translatedText")
 
     # Construye la consulta para la API de Afforai
-    query = f"{translated_name} guatemala ley"
+    query = f"{translated_question} guatemala leyes"
 
     # Realiza la solicitud a la API de Afforai
     response = requests.post(
@@ -34,49 +34,29 @@ def get_law_information(law_name):
     # Obtén el resultado de la API de Afforai
     result = json.loads(response.text)
 
-    # Busca la información de la ley
-    law_information = None
-    for link in result["data"]["links"]:
-        if link["link"] and "ley" in link["link"].lower():
-            law_information = link["link"]
+    # Busca la respuesta a la pregunta
+    answer = None
+    for message in result["data"]["messages"]:
+        if message["role"] == "assistant":
+            answer = message["content"]
             break
 
+    # Traducir la respuesta al inglés
+    translated_answer = translation_client.translate(answer, target="en").get("translatedText")
 
-    # Extrae la información de la ley
-    if law_information:
-        match = re.search(r"ley (\d+)", law_information)
-        if match:
-            law_number = int(match.group(1))
-            law_text = get_law_text(law_number)
-            st.write(law_text)
-        else:
-            st.error("No se encontró información sobre la ley.")
-    else:
-        st.error("No se encontró información sobre la ley.")
-    
-    # Función para obtener el texto de la ley
-    def get_law_text(law_number):
-        # Realiza una solicitud a la API de Afforai
-        response = requests.post(
-            "https://api.afforai.com/api/law_text",
-            json={
-                "apiKey": api_key,
-                "sessionID": session_id,
-                "lawNumber": law_number
-            }
-        )
-    
-        # Obtén el resultado de la API de Afforai
-        result = json.loads(response.text)
-    
-        # Devuelve el texto de la ley
-        return result["data"]["text"]
-    
-    # Inicia la aplicación Streamlit
-    st.title("Leyes de Guatemala")
-    st.write("Ingrese el nombre de la ley:")
-    law_name = st.text_input("Ley")
-    
-    # Busca la información de la ley
-    get_law_information(law_name)
-   
+    return translated_answer
+
+# Inicia la aplicación Streamlit
+st.title("Responder preguntas sobre las leyes de Guatemala")
+st.write("Ingrese su pregunta:")
+question = st.text_input("Pregunta")
+
+# Obtiene la respuesta a la pregunta
+answer = get_answer(question)
+
+# Muestra el resultado
+if answer:
+    st.success(answer)
+else:
+    st.error("Lo siento, no puedo responder a esa pregunta.")
+```
