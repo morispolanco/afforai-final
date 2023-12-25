28b95188c5d0e2796fae2abaa594d638720ffe0a
morispolanco <morispolanco@gmail.com>
2023-12-25 03:31:02 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,48 +1,64 @@
 import streamlit as st
 import requests
+from googleapiclient.discovery import build
+import json
 
-# Define the API endpoint
-api_endpoint = "https://datos.gob.gt/api/3/action/datastore_search"
+# Carga las credenciales de la API de Afforai
+api_key = "fcbfdfe8-e9ed-41f3-a7d8-b6587538e84e"
+session_id = "65489d7c9ad727940f2ab26f"
 
-# Set the resource ID
-resource_id = "leyes_publicadas"
+# Define la función para buscar el precio más bajo
+def get_lowest_price(product_name):
+    # Inicializa el cliente de Google Cloud Translation
+    translation_client = build("translate", "v3")
+
+    # Traducir el nombre del producto al español
+    translated_name = translation_client.translate(product_name, target="es").get("translatedText")
+
+    # Construye la consulta para la API de Afforai
+    query = f"{translated_name} precio guatemala"
+
+    # Realiza la solicitud a la API de Afforai
+    response = requests.post(
+        "https://api.afforai.com/api/api_completion",
+        json={
+            "apiKey": api_key,
+            "sessionID": session_id,
+            "history": [{"role": "user", "content": query}],
+            "powerful": True,
+            "google": True
+        }
+    )
+
+    # Obtén el resultado de la API de Afforai
+    result = json.loads(response.text)
+
+    # Busca el enlace con el precio más bajo
+    lowest_price_link = None
+    for link in result["data"]["links"]:
+        if link["link"] and "precio" in link["link"].lower():
+            lowest_price_link = link["link"]
+            break
+
+    # Extrae el precio más bajo
+    if lowest_price_link:
+        match = re.search(r"precio:\s*(\d+)", lowest_price_link)
+        if match:
+            return int(match.group(1))
+    return None
 
 # Inicia la aplicación Streamlit
 st.title("Responder preguntas sobre las leyes de Guatemala")
 st.write("Ingrese su pregunta:")
-question = st.text_input("Pregunta")
+product_name = st.text_input("Pregunta")
 
-# Procesa la pregunta
-processed_question = question.replace(" ", "+")
-
-# Define la función para obtener la respuesta a la pregunta
-def get_answer(question):
-    # Set the query parameters
-    query_params = {
-        "resource_id": resource_id,
-        "q": question,
-    }
-
-    # Make the API request
-    response = requests.get(api_endpoint, params=query_params)
-
-    # Get the JSON response
-    json_response = response.json()
-
-    # Busca la respuesta a la pregunta
-    answer = None
-    for result in json_response["result"]["records"]:
-        if result["titulo"].lower().find(question.lower()) != -1:
-            answer = result["contenido"]
-            break
-
-    return answer
-
-# Obtiene la respuesta a la pregunta
-answer = get_answer(processed_question)
+# Busca la respuesta
+response = get_response(product_name)
 
 # Muestra el resultado
-if answer:
-    st.success(answer)
+if response:
+    st.success(response)
 else:
-    st.error("Lo siento, no puedo responder a esa pregunta.")
+    st.error("No se encontró información sobre su pregunta.")
+else:
+    st.error(f"No se encontró información sobre el precio de {product_name} en Guatemala.")
