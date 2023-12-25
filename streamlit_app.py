358c0154970c0957fdba2ca2bc5edca7bbec752a
morispolanco <morispolanco@gmail.com>
2023-12-25 02:15:56 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,29 +1,41 @@
 import streamlit as st
 import requests
-import json
 
-# Replace with your own API key and session ID
-api_key = "fcbfdfe8-e9ed-41f3-a7d8-b6587538e84e"
-session_id = "65489d7c9ad727940f2ab26f"
+# Define la función para procesar la pregunta y obtener la respuesta
+def obtener_respuesta(pregunta):
+    try:
+        # Configura los parámetros de la API
+        api_key = "fcbfdfe8-e9ed-41f3-a7d8-b6587538e84e"
+        session_id = "65489d7c9ad727940f2ab26f"
+        url = "https://api.afforai.com/api/api_completion"
+        headers = {"Content-Type": "application/json"}
+        data = {
+            "apiKey": api_key,
+            "sessionID": session_id,
+            "history": [{"role": "user", "content": pregunta}],
+            "powerful": False,
+            "google": True
+        }
 
-def ask_question(question):
-    url = "https://api.afforai.com/api/api_completion"
-    data = {
-        "apiKey": api_key,
-        "sessionID": session_id,
-        "history": [{"role": "user", "content": question}],
-        "powerful": False,
-        "google": True
-    }
-    response = requests.post(url, json=data)
-    response_data = response.json()
-    answer = response_data["completions"][0]["content"]
-    return answer
+        # Realiza la solicitud POST a la API
+        response = requests.post(url, headers=headers, json=data)
+        response.raise_for_status()  # Lanza una excepción si la solicitud falla
 
-st.title("Preguntas sobre las leyes de Guatemala")
+        # Extrae la respuesta de la API
+        respuesta = response.json()["completions"][0]["data"]["content"]
 
-question = st.text_input("Introduce tu pregunta aquí:")
-if question:
-    answer = ask_question(question)
-    st.write("Respuesta:")
-    st.write(answer)
+        return respuesta
+
+    except Exception as e:
+        return f"Ocurrió un error al procesar la solicitud: {e}"
+
+
+# Interfaz de usuario de la aplicación
+st.title("Leyes de Guatemala")
+
+pregunta = st.text_input("Ingresa tu pregunta sobre las leyes de Guatemala:")
+
+if pregunta:
+    respuesta = obtener_respuesta(pregunta)
+    st.write("**Respuesta:**")
+    st.write(respuesta)
