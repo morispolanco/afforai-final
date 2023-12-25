28401f83de083824f3399bc812fed7cc7195caa5
morispolanco <morispolanco@gmail.com>
2023-12-25 03:25:40 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,46 +1,45 @@
 import streamlit as st
 import requests
 
-# Carga las credenciales de la API de Afforai
-api_key = "fcbfdfe8-e9ed-41f3-a7d8-b6587538e84e"
-session_id = "65489d7c9ad727940f2ab26f"
+# Define the API endpoint
+api_endpoint = "https://datos.gob.gt/api/3/action/datastore_search"
 
-# Define la función para responder preguntas sobre las leyes de Guatemala
-def get_answer(question):
-    # Construye la consulta para la API de Afforai
-    query = f"{question} guatemala leyes"
-
-    # Realiza la solicitud a la API de Afforai
-    response = requests.post(
-        "https://api.afforai.com/api/api_completion",
-        json={
-            "apiKey": api_key,
-            "sessionID": session_id,
-            "history": [{"role": "user", "content": query}],
-            "powerful": True,
-            "google": True
-        }
-    )
-
-    # Obtén el resultado de la API de Afforai
-    result = response.json()
-
-    # Busca la respuesta a la pregunta
-    answer = None
-    for message in result["data"]["messages"]:
-        if message["role"] == "assistant":
-            answer = message["content"]
-            break
-
-    return answer
+# Set the resource ID
+resource_id = "leyes_publicadas"
 
 # Inicia la aplicación Streamlit
 st.title("Responder preguntas sobre las leyes de Guatemala")
 st.write("Ingrese su pregunta:")
 question = st.text_input("Pregunta")
 
+# Procesa la pregunta
+processed_question = question.replace(" ", "+")
+
+# Define la función para obtener la respuesta a la pregunta
+def get_answer(question):
+    # Set the query parameters
+    query_params = {
+        "resource_id": resource_id,
+        "q": question,
+    }
+
+    # Make the API request
+    response = requests.get(api_endpoint, params=query_params)
+
+    # Get the JSON response
+    json_response = response.json()
+
+    # Busca la respuesta a la pregunta
+    answer = None
+    for result in json_response["result"]["records"]:
+        if result["titulo"].lower().find(question.lower()) != -1:
+            answer = result["contenido"]
+            break
+
+    return answer
+
 # Obtiene la respuesta a la pregunta
-answer = get_answer(question)
+answer = get_answer(processed_question)
 
 # Muestra el resultado
 if answer:
